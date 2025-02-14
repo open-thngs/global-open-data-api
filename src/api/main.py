0c34f1fc67d5430e3eb895e070bc4784909750df
Mirko Häberlin <mirko.haeberlin@gmail.com>
2025-02-14 15:21:53 +0100

    ADD: connect timescaledb + .env file usage

--- src/api/main.py
+++ src/api/main.py
@@ -1,15 +1,54 @@
 from typing import Union
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+import psycopg2 as timescaledb
+from dotenv import load_dotenv
+import os
 
-from fastapi import FastAPI
+# load .env-Datei
+load_dotenv()
+
+DB_USER = os.getenv("POSTGRES_USER")
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_PASS = os.getenv("POSTGRES_PASSWORD")
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_PORT = os.getenv("POSTGRES_PORT")
+
+DB_CONFIG = {
+    "host": DB_HOST,
+    "port": DB_PORT,
+    "dbname": DB_NAME,
+    "user": DB_USER,
+    "password": DB_PASS,
+}
 
 app = FastAPI()
 
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    favicon_path = os.path.join(os.getcwd(), "favicon.ico")
+    return FileResponse(favicon_path)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/ping")
+def ping():
+    try:
+        with timescaledb.connect(**DB_CONFIG) as db:
+            with db.cursor() as cursor:
+                cursor.execute("SELECT version();")
+                result = cursor.fetchone()
+        return {"message": "DB connected", "version": result}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/data")
+# def get_data():
+#     try:
+#         with timescaledb.connect(**DB_CONFIG) as db:
+#             with db.cursor() as cursor:
+#                 cursor.execute("SELECT * FROM example_table LIMIT 10;")  # Ersetze mit deiner Tabelle
+#                 rows = cursor.fetchall()
+#         return {"data": rows}
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
+
